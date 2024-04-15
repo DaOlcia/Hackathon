f215e420263e874322ac46a7d3a546c797f5b164
Gaby <137078206+GvGurp@users.noreply.github.com>
2024-04-15 13:23:32 +0200

    Navigatie gemaakt
    Navigatie gemaakt en in elke pagina geplaast

--- FormulierRegistratie.php
+++ FormulierRegistratie.php
@@ -1,3 +1,9 @@
+<?php 
+require_once "Conn.php";
+session_start (); 
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,14 +11,46 @@
     <script src="https://kit.fontawesome.com/ecd9dddd0d.js" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Registreren</title>
+    <link rel="stylesheet" href="NavCss.css"> 
     <link rel="stylesheet" href="Main.css">
 </head>
 <body>
-<nav> 
-    <ul> <li><a href=>Hoofdpagina</a></li> </ul>
-    <ul> <li><a href=>Hoofdpagina</a></li> </ul>
-    <ul> <li><a href=>Hoofdpagina</a></li> </ul> 
-</nav>
+<nav>
+    <ul> <li><a href="Hoofdpagina.php">Hoofdpagina</a></li> </ul> 
+        <div class="Dropdown">
+            <button class="NavButton1"><i class="fa-solid fa-star"></i>Registreer</button>
+                 <div class="SubDropdown1">
+                     <button class="NavButton2">Bijna</button>
+                        <div class="SubDropdown2">
+                            <button class="NavButton3">Nog 1</button>
+                                <div class="SubDropdown3">
+                                    <button class="NavButton4"> Haha lozer</button>
+                                         <ul class="SubDropdown4"> 
+                                              <li> <a href="FormulierRegistratie.php"> Klik hier</a> </i>
+                                          </ul>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                  </div>
+
+                  <div class="Dropdown">
+            <button class="NavButton1">Log in</button>
+                 <div class="SubDropdown1">
+                     <button class="NavButton2">Inloggen??</button>
+                        <div class="SubDropdown2">
+                            <button class="NavButton3">Waarom?</button>
+                                <div class="SubDropdown3">
+                                    <button class="NavButton4"> Kan toch niet</button>
+                                         <ul class="SubDropdown4"> 
+                                              <li> <a href="FormulierRegistratie.php"> Registreer Sukkeltje</a> </i>
+                                          </ul>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                  </div>
+    </nav>
 
     <h1> Registratie formulier </h1> 
     <p> Vul de formulier hieronder in om te registeren. </p>
---
+++ Hoofdpagina.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ecd9dddd0d.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/ecd9dddd0d.js" crossorigin="anonymous"></script>
+    <title>Hoofdpagina</title>
+    <link rel="stylesheet" href="NavCss.css"> 
+    <link rel="stylesheet" href="Main.css">
+</head>
+<body>
+    <nav>
+    <ul> <i class="fa-solid fa-star"></i><li><a href="Hoofdpagina.php">Hoofdpagina</a></li> </ul> 
+        <div class="Dropdown">
+            <button class="NavButton1">Registreer</button>
+                 <div class="SubDropdown1">
+                     <button class="NavButton2">Bijna</button>
+                        <div class="SubDropdown2">
+                            <button class="NavButton3">Nog 1</button>
+                                <div class="SubDropdown3">
+                                    <button class="NavButton4"> Haha lozer</button>
+                                         <ul class="SubDropdown4"> 
+                                              <li> <a href="FormulierRegistratie.php">  Registreer hier Sukkeltje </a> </i>
+                                          </ul>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                  </div>
+
+                  <div class="Dropdown">
+            <button class="NavButton1">Log in</button>
+                 <div class="SubDropdown1">
+                     <button class="NavButton2">Inloggen??</button>
+                        <div class="SubDropdown2">
+                            <button class="NavButton3">Waarom?</button>
+                                <div class="SubDropdown3">
+                                    <button class="NavButton4"> Kan toch niet</button>
+                                         <ul class="SubDropdown4"> 
+                                              <li> <a href="FormulierRegistratie.php"> Registreer Sukkeltje</a> </i>
+                                          </ul>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                  </div>
+    </nav>
+
+        
+</body>
+</html>
